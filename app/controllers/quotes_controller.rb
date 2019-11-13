c3dad7e23152f9a8b4f0c37c24f536c73566b200
Majun1990 <merijun_a@hotmail.com>
2019-11-13 15:32:36 +0000

    added validation on saying and author

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,6 +8,16 @@ class QuotesController < ApplicationController
   end
 
   def create
+    @quote=Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error]='<strong>Cannot save</strong> the data you entered is invalid.'
+    end
+    redirect_to root_path
+  end
 
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
   end
 end
